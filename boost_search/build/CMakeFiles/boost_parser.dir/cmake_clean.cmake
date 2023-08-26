2b0758e35206ca391e01139dc0f5d0729ea73615
Sirudoi <sirudoi@163.com>
2023-08-27 01:06:02 +0800

    BoostSearch -- Add cmake file

---
+++ boost_search/build/CMakeFiles/boost_parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_parser.dir/parser.cc.o"
+  "boost_parser.pdb"
+  "boost_parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ boost_search/build/CMakeFiles/boost_searcher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_searcher.dir/search.cc.o"
+  "boost_searcher.pdb"
+  "boost_searcher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost_searcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
